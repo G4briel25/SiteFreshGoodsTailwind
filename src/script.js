81b850584e622f058576e93bad05a33ffa8f9149
Gabriel Jaune <warmerishoth@gmail.com>
2023-10-09 14:48:55 -0400

    update botao no projeto

--- src/script.js
+++ src/script.js
@@ -3,4 +3,24 @@ const menu = document.getElementById('exibirMenu');
 
 botao.addEventListener('click', ()=>{
     menu.classList.toggle('hidden');
-})
+});
+
+// ----------------------------------------------------
+
+const btn = document.getElementById('btnTop');
+
+btn.addEventListener('click', ()=> {
+    window.scrollTo(0,0);
+});
+
+document.addEventListener('scroll', ocultar);
+
+function ocultar() {
+    if(window.scrollY > 10) {
+        btn.style.display = 'flex';
+    } else {
+        btn.style.display = 'none';
+    };
+};
+
+ocultar();
